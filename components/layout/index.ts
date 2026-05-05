a3aed2fa58b6781563eb42371e044152d9802fe8
Alex <alexprogran@gmail.com>
2026-05-05 00:26:10 -0300

    Ajuste do index

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -1,3 +1,3 @@
 export { default as BottomNav } from "./BottomNav";
-export { default as HeaderSection } from "./HeaderSection";
+export { default as HeaderSection } from "../sections/HeaderSection";
 export { default as Providers } from "./providers";
